738464326062d8655ca4896b8c85a5f1af55f739
dalya kablawi <dalyakablawi@gmail.com>
2024-10-21 09:38:27 -0400

    happy wall html touch ups

--- assets/js/inlove-script.js
+++ assets/js/inlove-script.js
@@ -7,7 +7,6 @@ const scribeMsg = document.getElementById("scribe-msg");
 const form = document.getElementById("add-vibe-scribe");
 const container = document.querySelector(".cards-container");
 
-// 1. Display: none the button, 2. pop up model with date, title, thoughts 3. when submit button is clicked, store in local storage as string and 4. render title, thoughts under same flex container 5. when submit is clicked, new button is rendered in a newly created div flex-item //
 
 function addVibeScribe() {
   document.getElementById("add-vibe-scribe").style.display = "block";
